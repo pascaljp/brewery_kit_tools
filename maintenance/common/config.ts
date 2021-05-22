c65687fa9e512dc63d77cfca6d8b12e2c6cbed50
Koichi Suematsu <pascal.jp@gmail.com>
2021-05-23 07:20:08 +0900

    Create required directories before accessing them

--- maintenance/common/config.ts
+++ maintenance/common/config.ts
@@ -2,11 +2,11 @@ import * as os from 'os';
 import * as path from 'path';
 
 function getConfigPath(): string {
-  return path.join(os.homedir(), '.inkbird', 'config.json');
+  return path.join(os.homedir(), 'config.json');
 }
 
 function getDataDir(): string {
-  return path.join(os.homedir(), '.inkbird', 'data');
+  return path.join(os.homedir(), 'data');
 }
 
 export {getConfigPath, getDataDir};
